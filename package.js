eab69b427d44881561b74b829125f30bafbe0caa
Smetech <smetech@mailbox.sme001.com>
2020-05-04 09:45:27 +0200

    back to mong-id 1.0.5

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: 'Mylar, meteor changes',
-    version: '0.3.2',
+    version: '0.3.2_1',
     name: 'shjiaye:meteor-changes',
     git: 'https://github.com/carrot93/mylar-meteor-changes.git'
 });
@@ -9,7 +9,7 @@ Package.onUse(function (api) {
     api.versionsFrom('METEOR@1.0');
 
     api.use(['ddp', 'random', 'underscore', 'mongo', 'minimongo', 'ejson',
-             'dburles:mongo-collection-instances@0.2.6','mongo-id@1.0.7'], ['client', 'server']);
+             'dburles:mongo-collection-instances@0.2.6','mongo-id@1.0.5'], ['client', 'server']);
 
     api.addFiles(['helpers.js', 'mongo-ddp-adaptor.js', 'ddp-framework.js'], ['client', 'server']);
 });
